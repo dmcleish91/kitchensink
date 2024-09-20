d5a962c2cc95959533dcd82b73bbf35fad52b5a6
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2024-09-20 13:53:36 -0400

    feat: removed for demo

--- src/pages/kitchensink.tsx
+++ src/pages/kitchensink.tsx
@@ -1,21 +1,13 @@
-import { useState } from 'react';
-
 function KitchenSink() {
-  const [count, setCount] = useState(0);
-
-  function incrementCount() {
-    setCount(count + 1);
-  }
-
   return (
     <div style={{ border: '2px solid gray', padding: '20px', textAlign: 'center' }}>
       <div style={{ fontSize: '50px', marginBottom: '20px' }}>
         🧼 🛁 <br />
-        <span style={{ fontSize: '24px' }}>Cartoon Kitchen Sink</span>
+        <span style={{ fontSize: '24px' }}>Kitchen Sink</span>
       </div>
       <div>
-        <p>Count: {count}</p>
-        <button onClick={incrementCount}>Increment</button>
+        <p>Count:</p>
+        <button>Increment</button>
       </div>
     </div>
   );
